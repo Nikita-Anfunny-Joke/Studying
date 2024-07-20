eb0222bf4bfbdfee393cdec10bade429b6b624d2
Nikita Anosov <anosovnik@mail.ru>
2024-07-20 15:36:56 +0300

    Data_types

---
+++ Studying.py
@@ -0,0 +1,31 @@
+"""
+Здесь я буду изучать Python и машинное обучение
+"""
+
+# ТИПЫ ДАННЫХ
+""" НЕИЗМЕЯЕМЫЕ ТИПЫ ДАННЫХ
+1) строка - str
+2) целые числа - int
+3) числа с 'плавающей' точкой - float
+4) кортежи - tuple
+5) логические типы данных - bool
+"""
+my_string = "Hello"
+print(type(my_string))
+
+number = 3
+print(type(number))
+
+float_number = 3.141592
+print(type(float_number))
+
+my_tuple = ("Strings", "Numbers", "Tuples", "Boolean")
+print(type(my_tuple))
+
+logical_val = True
+print(type(logical_val))
+
+logical_val = False
+print(type(logical_val))
+
+
