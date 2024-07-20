63831fd706455103b6b16e3bb283942569e998c3
Nikita Anosov <anosovnik@mail.ru>
2024-07-20 16:32:00 +0300

    Tasks_1

---
+++ main.py
@@ -0,0 +1,50 @@
+""" ЗАДАНИЯ ПО КУРСУ """
+
+# Создаю переменные с именем, возрастом и булевым выражением
+name = "Nikita"
+age = 27
+is_student = True
+print(name, age, is_student)
+
+
+
+total_money = 1000
+price = 26
+jar_count = total_money // price
+print(jar_count)
+
+tasks = {
+    1 : ['Полить цветы', 'Забрать посылку'],
+    0 : ['Покормить кота'],
+    2 : ['Почитать книгу по программированию'],
+    3 : ['Ответить на письмо двоюродной тети']
+}
+print(tasks)
+
+
+for keys in tasks:
+    if 0 in tasks:
+        print("есть срочные дела")
+    else:
+        print("можно отдохнуть")
+
+values = []
+for keys in tasks:
+    values.append(tasks[keys])
+print(values)
+
+doings = []
+for keys in tasks:
+    for doin in tasks[keys]:
+        doings.append(doin)
+
+print(doings)
+"""
+doings = []
+
+for a in tasks.values():
+    for b in a:
+        doings.append(b)
+doings
+
+"""
