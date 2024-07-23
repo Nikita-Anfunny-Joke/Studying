3d9ac3793e25e2770c84725db951fedac9ad7f1c
Nikita Anosov <anosovnik@mail.ru>
2024-07-23 19:39:35 +0300

    Tasks_func

--- main.py
+++ main.py
@@ -210,5 +210,15 @@ def find_substr(unstr, full_str):
         return (full_str.index(first_znac), full_str.index(last_znac) +1)
     else:
         pass
-    
+
+find_substr("ма", "маленькая машина")
+
+
+def find_substr(substring, string):
+    for i in range(len(string) - len(substring) + 1):
+
+        candidate = string[i:i + len(substring)]
+        if candidate == substring:
+            return (i, i + len(substring))
+
 find_substr("ма", "маленькая машина")
