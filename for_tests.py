6e0a0dfc02fabe0c082528ed41df95fac281af68
Nikita Anosov <anosovnik@mail.ru>
2024-07-23 21:47:32 +0300

    Tasks_func

--- for_tests.py
+++ for_tests.py
@@ -1,18 +1,10 @@
 
-
-test1 = ['e',6,8,'A','>','^','S','$','R','C',6,'+','#',9,'/',1,'T','!','%','K',7,'-','O','*','<',2,'h',4,'g']
-
-print(len(test1))
 answer = []
-
-def fifth_element(full_list: list) -> list:
-    for n in range(len(full_list) // 5):
-        print(n)
-        answer.append(full_list[(len(full_list)) - (5 * (n + 1))])
+def process_string(string):
+    answer = string[1:].lower()
+    if 'intern' in answer:
+        answer = answer.replace('intern', 'junior')
         print(answer)
     return answer
 
-
-
-
-fifth_element(test1)
+process_string('IIntern reads a lot of books')
--- main.py
+++ main.py
@@ -238,3 +238,16 @@ def fifth_element(full_list: list) -> list:
     return answer
 
 fifth_element(test1)
+
+
+# ЗАДАЧА С СМЕНОЙ СЛОВ И ИЗМЕНЕНИЕМ РЕГИСТРА
+
+answer = []
+def process_string(string):
+    answer = string[1:].lower()
+    if 'intern' in answer:
+        answer = answer.replace('intern', 'junior')
+        print(answer)
+    return answer
+
+process_string('IIntern reads a lot of books')
