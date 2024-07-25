61125c4b8c7b8fddcdfb52e16619a0d23c8641f2
Nikita Anosov <anosovnik@mail.ru>
2024-07-25 21:11:00 +0300

    Tasks-DB

--- for_tests.py
+++ for_tests.py
@@ -1,8 +1,20 @@
 
-from datetime import timedelta,datetime
 
-times = datetime(month=3, day=8, year=2022) + timedelta(days=3, hours=5, minutes=6)
+# pip install psycopg2 если библиотека не установлена
+import psycopg2
 
+database="startml",
+user="robot-startml-ro",
+password="pheiph0hahj1Vaif",
+host="postgres.lab.karpov.courses",
+port=6432
 
+import pandas as pd
 
+df = pd.read_sql(
+    """SELECT * FROM "feed_action" LIMIT 10 """,
+    con="postgresql://robot-startml-ro:pheiph0hahj1Vaif@"
+        "postgres.lab.karpov.courses:6432/startml"
+)
 
+df.head(5)
